1383e649661fe79f93fb7ad94c2dd22496626977
Mark Sirany <marksirany@me.com>
2020-01-22 15:36:37 -0600

    added code from brian

--- server.js
+++ server.js
@@ -5,32 +5,19 @@ const mongojs = require("mongojs");
 
 const PORT = process.env.PORT || 3000;
 
-const Workout = require("./models/workout");
-
 const app = express();
-
 
 app.use(logger("dev"));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static("public"));
 
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workoutdb", { useNewUrlParser: true });
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", { useNewUrlParser: true });
 
 const db = require("./models");
 
-//get last workout
-app.get("/api/workouts", (req, res) => {
-    db.Workout.find({})
-        .then(workout => {
-            res.json(workout);
-        })
-        .catch(err => {
-            res.json(err);
-        });
-});
-
-
+require("./routes/apiRoutes")(app);
+require("./routes/htmlRoutes")(app);
 
 app.listen(PORT, () => {
     console.log(`App running on port ${PORT}!`);
